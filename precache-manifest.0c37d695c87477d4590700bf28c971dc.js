079f8cd098ac2eda178785d42b663c42e15393da
Idris <idrist2013@gmail.com>
2020-02-11 02:18:22 +0000

    Updates

---
+++ precache-manifest.0c37d695c87477d4590700bf28c971dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51ebe7cd9c843c8256806b6ab970a2e",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "e498f926441a1f8a065b",
+    "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a531a0928fcf6e10c13d",
+    "url": "/robotfriends/static/css/main.841e44e1.chunk.css"
+  },
+  {
+    "revision": "e498f926441a1f8a065b",
+    "url": "/robotfriends/static/js/2.fe68dc42.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robotfriends/static/js/2.fe68dc42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a531a0928fcf6e10c13d",
+    "url": "/robotfriends/static/js/main.f4b32a5d.chunk.js"
+  },
+  {
+    "revision": "42022b9ab7029de85b55",
+    "url": "/robotfriends/static/js/runtime-main.f98b13f7.js"
+  }
+]);
